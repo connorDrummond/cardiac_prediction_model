c1873a4cdf5ff396a43a63d887c37c26c1b1356a
connorDrummond <cdrummond31696@gmail.com>
2025-07-17 17:54:18 -0500

    Created user input sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,11 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 
 
+st.title("Coronary Artery Disease Predictor")
+st.write(
+    "Enter your most recent cardiology lab results to get your results now!"
+)
+
 dataset = pd.read_csv('cardiac_arrest_dataset.csv')
 
 # create our training, and validation data. Randomly choose which data is in which set.
@@ -40,9 +45,20 @@ model.fit(x_train_scaled, y_train, epochs=40, batch_size=32, validation_split=0.
 model.evaluate(x_test_scaled, y_test, verbose=2)
 
 
-st.title("Coronary Artery Disease Predictor")
-st.write(
-    "Enter your most recent cardiology lab results to get your results now!"
-)
+# create the front-end to collect user data
+
+with st.sidebar:
+    st.header("Please enter your lab data")
+    age = st.number_input("Please enter your age")
+    sex = st.selectbox('Gender', ('Male', 'Female'))
+    chest_pain = st.selectbox('Chest pain type', ('None', 'Typical angina', 'Atypical angina','Non-anginal pain', 'Asymptomatic'))
+    blood_sugar = st.selectbox('Is your blood sugar > 120 mg/dl?', ('Yes', 'No'))
+    restecg = st.selectbox('Resting ECG results', ('Normal', 'ST-T wave abnormality (e.g., T wave inversion, ST elevation/depression > 0.05 mV)', "Left ventricular hypertrophy (by Estes' criteria"))
+    exang = st.selectbox('Exercise-related angina', ('Yes', 'No'))
+    st_slope = st.selectbox('ST Slope', ('Upsloping', 'Flat', 'Downsloping'))
+    cholesterol = st.number_input("Cholesterol results", min_value=100, max_value=300)
+    restbps = st.number_input("Resting systolic blood pressure results", min_value=80, max_value=250)
+    max_heart_rate = st.number_input("Maximum exercise-induced heart rate results", min_value=60, max_value=300)
+    oldpeak = st.number_input("ST Depression (Oldpeak)", min_value=0, max_value=50)
 
 
