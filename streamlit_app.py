014eee3d41fd50136b31548fd7db684d498a9320
connorDrummond <cdrummond31696@gmail.com>
2025-07-20 08:24:34 -0500

    Bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,36 +34,37 @@ input_df
 
 # Below is the code for the model. It has already been trained and saved as 'heart_disease_predicition_model.h5'
 
-"""dataset = pd.read_csv('cardiac_arrest_dataset.csv')
+dataset = pd.read_csv('cardiac_arrest_dataset.csv')
 
 # create our training, and validation data. Randomly choose which data is in which set.
-x = dataset[['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak','slope' ]]
-y = dataset['target']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+#x = dataset[['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak','slope' ]]
+#y = dataset['target']
+
+#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # create the model
-model = tf.keras.models.Sequential()
+#model = tf.keras.models.Sequential()
 
 # scale the input to fit the model
-scaler = StandardScaler()
-x_train_scaled = scaler.fit_transform(x_train)
-x_test_scaled = scaler.fit_transform(x_test)
+#scaler = StandardScaler()
+#x_train_scaled = scaler.fit_transform(x_train)
+#x_test_scaled = scaler.fit_transform(x_test)
 
 #print(x_train_scaled.shape)
 # add layers to the model
-model.add(keras.layers.Dense(units=64, activation='relu', input_shape=(x_train_scaled.shape[1],)))
-model.add(keras.layers.Dense(8, input_dim= 5, activation='relu'))  # Adjust number of neurons if needed
-model.add(keras.layers.Dense(1, activation='linear'))
+#model.add(keras.layers.Dense(units=64, activation='relu', input_shape=(x_train_scaled.shape[1],)))
+#model.add(keras.layers.Dense(8, input_dim= 5, activation='relu'))  # Adjust number of neurons if needed
+#model.add(keras.layers.Dense(1, activation='linear'))
 
 # Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+#model.compile(optimizer='adam', loss='mean_squared_error')
 
 # train the model
-model.fit(x_train_scaled, y_train, epochs=40, batch_size=32, validation_split=0.2, verbose=2)
+#model.fit(x_train_scaled, y_train, epochs=40, batch_size=32, validation_split=0.2, verbose=2)
 
 # evaluate the model.
-model.evaluate(x_test_scaled, y_test, verbose=2)"""
+#model.evaluate(x_test_scaled, y_test, verbose=2)
 
 
 
