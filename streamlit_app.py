f9fe1005a9a9773202d8d58a544dd07487466255
cdrum38 <cdrum38@wgu.edu>
2025-07-20 12:07:33 -0500

    Bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,5 +81,5 @@ dataset_scaled = scaler.fit_transform(dataset)
 prediction_model = tf.keras.models.load_model('heart_disease_prediction_model.h5')
 
 if st.button("Predict"):
-    prediction = prediction_model.predict(dataset_scaled[-1])
+    prediction = prediction_model.predict(dataset_scaled)
     st.write(f"Your likelihood of Coronary Artery Disease: {prediction}")
