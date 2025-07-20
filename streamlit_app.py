f5682f9bd14aeec739eac3f567da119dcc8641af
connorDrummond <cdrummond31696@gmail.com>
2025-07-20 16:24:28 -0500

    Bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,12 +122,12 @@ scaler = StandardScaler()
 #
 
 prediction_model = tf.keras.models.load_model('heart_disease_prediction_model.h5')
+dataset.drop(columns=['ca', 'thal', 'target'], inplace=True)
+dataset = pd.concat([dataset, input_df], axis=0)
+dataset_scaled = scaler.fit_transform(dataset)
+prediction = prediction_model.predict(dataset_scaled)
 
+dataset['prediction'] = prediction
 if st.button("Predict"):
-
-    dataset.drop(columns=['ca', 'thal', 'target'], inplace=True)
-    dataset = pd.concat([dataset, input_df], axis=0)
-    dataset_scaled = scaler.fit_transform(dataset)
-    prediction = prediction_model.predict(dataset_scaled)
     st.write(f"Your likelihood of Coronary Artery Disease: {prediction}")
 
