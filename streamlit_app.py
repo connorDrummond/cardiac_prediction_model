e03893c9811f60cc9847a841f4fd41a2e45a1850
connorDrummond <cdrummond31696@gmail.com>
2025-07-21 09:07:57 -0500

    bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,9 +136,9 @@ prediction = prediction_model.predict(dataset_scaled)
 ## As the model can predict values slightly lower than 0 or slightly higher than 1, we will scale the extreme ends of prediction to < .05 and >.95. This will alleviate user confusion.
 for x in prediction:
     if x >= .95:
-         prediction[x] = .95
+         x = .95
     elif x <= .05:
-        prediction[x] = .05
+        x = .05
 
 st.write(prediction)
 
